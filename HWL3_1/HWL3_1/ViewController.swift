c73e2ba446fb29abfc2baf46af68eed1061e5451
DanilEVG <89922067067@mail.ru>
2024-04-07 12:41:05 +0500

    HWL3_1

---
+++ HWL3_1/HWL3_1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HWL3_1
+//
+//  Created by Даниил Евгеньевич on 07.04.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
